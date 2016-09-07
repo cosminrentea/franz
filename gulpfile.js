dc86207a5b1c5f92d5eb9c5a32a2930dc631dd52
Cosmin Rentea <c.rentea@tarent.de>
2016-09-07 12:53:17 +0300

    adding jshint, many npm devDeps, env as dev/prod, minification (still not working)

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,44 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var gutil = require('gulp-util');
+var gulpif = require('gulp-if');
+var jshint= require('gulp-jshint');
 
 // Define paths.
 var paths = {
-    js: ['./app/franz.js']
+    js: ['./app/franz.js'],
+    dest: './static/js/'
 };
+
+var isProd = false;
+
+// If "--env=prod" is passed from the command line then update the flags
+if (gutil.env.env === 'prod') {
+    isProd = true;
+}
+
+var isDev = !isProd;
 
 // The JS task.
 gulp.task('js', function () {
-    return browserify({
+    var b = browserify({
         entries: paths.js,
         debug: true
-    }).bundle()
+    });
+    return b.bundle()
         .pipe(source('bundle.js'))
-        .pipe(gulp.dest('./static/js/'));
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'), { verbose: true })
+        .pipe(jshint.reporter('fail'))
+        .pipe(gulpif(isDev, gulp.dest(paths.dest)))
+        .pipe(buffer())
+        .pipe(gulpif(isProd, uglify()))
+        .pipe(gulpif(isProd, gulp.dest(paths.dest)));
 });
 
 // Rerun tasks whenever a file changes.
